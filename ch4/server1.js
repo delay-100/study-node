19653b3334bc0b77fb61c2e3d9982cc61e51f7d7
delay100 <qor3127@naver.com>
2022-01-15 00:00:53 +0900

    ch4_http server

--- ch4/server1.js
+++ ch4/server1.js
@@ -5,6 +5,7 @@ http.createServer((req,res)=> {
     res.write('<h1>Hello Node!</h1>');
     res.end('<p>Hello Server!</p>');
 })
- .listen(3000, () => { // 서버연결
+// 이벤트 리스너 생성, 서버를 계속 열어둠!
+ .listen(8080, () => { // 서버연결
     console.log('8080번 포트에서 서버 대기 중입니다!');
 });
