7b43de477bc4999e3f58f470eff2277da5e5e5f3
delay100 <qor3127@naver.com>
2022-01-17 23:57:50 +0900

    express, middleware -2

--- ch6/middleware/app.js
+++ ch6/middleware/app.js
@@ -31,16 +31,22 @@ app.use(cookieParser(process.env.COOKIE_SECRET)); // 비밀키 할당, process.e
 // res.clearCookie('name', 'delay100', {httpOnly: true, secure: true});
 
 // express-session
+// 인수: session에 대한 설정
 app.use(session({
-    resave:false,
-    saveUninitialized: false,
+    resave:false, // resave : 요청이 올 때 세션에 수정 사항이 생기지 않더라도 세션을 다시 저장할지 설정
+    saveUninitialized: false, // saveUninitialized : 세션에 저장할 내역이 없더라도 처음부터 세션을 생성할지 설정
     secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
+    cookie: { // 세션 쿠키에 대한 설정
+        httpOnly: true, // httpOnly: 클라이언트에서 쿠키를 확인하지 못하게 함
+        secure: false, // secure: false는 https가 아닌 환경에서도 사용 가능 - 배포할 때는 true로 
+                        // 코드에는 없지만, store: 데이터베이스에 연결해서 세션을 유지 
     },
     name: 'session-cookie',
 }));
+
+// req.session.name = 'delay100'; // 세션 등록
+// req.sessionID; //세션 아이디 확인
+// req.session.destroy(); // 세션 모두 제거
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
