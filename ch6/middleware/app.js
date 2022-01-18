d3d4396c1f9aba721c66db1ea7faa5185966ea44
delay100 <qor3127@naver.com>
2022-01-18 16:55:16 +0900

    multer, router

--- ch6/middleware/app.js
+++ ch6/middleware/app.js
@@ -6,6 +6,11 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+
+// 라우터 분리
+const indexRouter = require('./routes'); 
+const userRouter = require('./routes/user');
+
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -44,9 +49,75 @@ app.use(session({
     name: 'session-cookie',
 }));
 
+// 라우터 경로 설정
+app.use('/', indexRouter); // localhost:3000
+app.use('/user', userRouter); // localhost:3000/user
+
+// multer
+const multer = require('multer');
+const fs = require('fs');
+
+try {
+    fs.readdirSync('uploads'); // sync로 upload 폴더 읽음
+}catch(error){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다');
+    fs.mkdirSync('uploads'); // upload 폴더가 없으면 upload가 불가능하므로 폴더 생성
+}
+const upload = multer({ // multer의 인수로 설정을 넣음
+    storage: multer.diskStorage({ // storage(저장 위치) 속성
+        destination(req, file, done){ // 어디?(요청 정보, 업로드한 파일 정보, 함수) -> req, file 데이터를 가공 후 done으로 넘김
+            done(null, 'uploads/'); // done(에러가 있으면 에러를 넣음, 실제 경로(or 파일 이름))
+        },
+        filename(req, file, done){ // 파일 이름
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext); // 이름이 겹치지 않도록 이름에 현재 시간을 저장 
+            // 결국, upload라는 폴더에 [파일명+현재시간.확장자]로 업로드
+        },
+    }),
+    limits: {fileSize: 5 * 1024 * 1024}, // 업로드 제한 사항:{파일 크기: 5mb}
+});
+app.get('/upload', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'multipart2.html'));
+});
+app.post('/upload', upload.fields([{name: 'image1'},{name: 'image2'}]),
+    (req, res) => {
+        console.log(req.files, req.body);
+        res.send('ok');
+    },
+);
+
+// 파일을 하나만 업로드하는 방법 (multipart.html)
+// app.post('/upload', upload.single('image'), (req, res) => {
+//     console.log(req.file, req.body);
+//     res.send('ok');
+//   });
+
+// 여러 파일을 업로드하는 방법 (multipart2.html)
+// app.post('/upload', upload.array('many', (req,res) => {
+//     console.log(req.files, req.body);
+//     res.send('ok');
+// }))
+
+// 여러 파일을 업로드하는 방법2 (multipart2.html)
+// app.post('/upload', upload.fields([{name: 'image1'}, {name: 'image2'}]),(req, res) => {
+//     console.log(req.files, req.body);
+//     res.send('ok');
+// },
+// );
+
+// 파일을 업로드하지 않고 멀티파트 형식으로 업로드 방법 (multipart3.html)
+// app.post('/upload', upload.none(), (req, res)=> {
+//     console.log(req.body); // 파일을 업로드하지 않았으므로 req.body만 존재
+//     res.send('ok');
+// });
+
 // req.session.name = 'delay100'; // 세션 등록
 // req.sessionID; //세션 아이디 확인
 // req.session.destroy(); // 세션 모두 제거
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found'); // 일치하는 라우터가 없을 때 404 상태 코드를 응답
+});
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
@@ -61,7 +132,7 @@ app.get('/',(req, res, next) => {
     console.log('GET / 요청에서만 실행됩니다.');
     next();
 }, (req, res) => { // 요청이 오면 무조건 에러 처리
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.') 
+ //   throw new Error('에러는 에러 처리 미들웨어로 갑니다.') 
 });
 
 app.use((err, req, res, next) =>{
