ab4c0a8028ada7fa1674a753e9bf457fdc9a76b6
delay100 <qor3127@naver.com>
2022-01-31 10:11:24 +0900

    add middleware, router

--- ch9/sns3/routes/page.js
+++ ch9/sns3/routes/page.js
@@ -1,11 +1,13 @@
 // app.js에서 기본 router로 설정한 page.js
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares'); // 구조분해할당으로 middlewares의 두 미들웨어를 가져옴
 
 const router = express.Router();
 
 // 모든 요청마다 실행
 router.use((req,res,next)=>{
-    res.locals.user = null;  // res.locals는 변수를 모든 템플릿 엔진에서 공통으로 사용, 즉 user는 전역 변수로 이해하면 됨(아래도 동일)
+    // res.locals.user = null;  // res.locals는 변수를 모든 템플릿 엔진에서 공통으로 사용, 즉 user는 전역 변수로 이해하면 됨(아래도 동일)
+    res.locals.user = req.user; // 요청으로 온 유저를 넌적스에 연결
     res.locals.followerCount = 0;
     res.locals.followingCount = 0;
     res.locals.followerIdList = [];
@@ -13,12 +15,12 @@ router.use((req,res,next)=>{
 });
 
 // http://127.0.0.1:8001/profile 에 get요청이 왔을 때 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile', { title: '내 정보 - sns'});
 });
 
 // http://127.0.0.1:8001/join 에 get요청이 왔을 때 
-router.get('/join', (req, res)=>{
+router.get('/join', isNotLoggedIn, (req, res)=>{
     res.render('join', {title: '회원가입 - sns'});
 });
 
