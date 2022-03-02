c18e80d8e297ca8b496e5d45dbd8af5677707e37
delay100 <qor3127@naver.com>
2022-03-02 09:19:03 +0900

    add socket.io module and update socket.js

--- ch12/gif-chat/socket.js
+++ ch12/gif-chat/socket.js
@@ -1,26 +1,52 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
+    const io = new SocketIO(server, { path: '/socket.io' });
 
-    wss.on('connection', (ws, req) => { // 웹 소켓 연결 시
+    io.on('connection', (socket) => { // 웹 소켓 연결 시
+        const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => { // 클라이언트로부터 메시지 수신 시
-            console.log(message);
+        console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
+        socket.on('disconnect', () => { // 연결 종료 시
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
         });
-        ws.on('error', (error) => { // 에러 시
+        socket.on('error', (error) => { // 에러 시
             console.error(error);
         });
-        ws.on('close', () => { // 연결 종료 시
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
+        socket.on('reply', (data) => { // 클라이언트로부터 메시지 수신 시
+            console.log(data);
         });
 
-        ws.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
-            if(ws.readyState == ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-            }
+        socket.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
+           socket.emit('news', 'Hello Socket.IO');
         }, 3000);
     });
 };
+
+// const WebSocket = require('ws');
+
+// module.exports = (server) => {
+//     const wss = new WebSocket.Server({ server });
+
+//     wss.on('connection', (ws, req) => { // 웹 소켓 연결 시
+//         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+//         console.log('새로운 클라이언트 접속', ip);
+//         ws.on('message', (message) => { // 클라이언트로부터 메시지 수신 시
+//             console.log(message);
+//         });
+//         ws.on('error', (error) => { // 에러 시
+//             console.error(error);
+//         });
+//         ws.on('close', () => { // 연결 종료 시
+//             console.log('클라이언트 접속 해제', ip);
+//             clearInterval(ws.interval);
+//         });
+
+//         ws.interval = setInterval(() => { // 3초마다 클라이언트로 메시지 전송
+//             if(ws.readyState == ws.OPEN) {
+//                 ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
+//             }
+//         }, 3000);
+//     });
+// };
